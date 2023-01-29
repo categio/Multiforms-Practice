1a4394a08d1e91179f0c44d0c684a99c99067501
Categio <categio1@gmail.com>
2023-01-28 21:22:16 -0800

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiforms_Practice
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void displayFormButton_Click(object sender, EventArgs e)
+        {
+            //create new messageform obj
+            MessageForm myMessageForm = new MessageForm();
+
+            //display the new messageform object
+            myMessageForm.ShowDialog();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+    }
+}
---
+++ MessageForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Multiforms_Practice
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(113, 96);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(139, 58);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "&Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.messageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.messageLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.messageLabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.messageLabel.Location = new System.Drawing.Point(58, 21);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(253, 55);
+            this.messageLabel.TabIndex = 1;
+            this.messageLabel.Text = "Have  a great day!";
+            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 173);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.closeButton);
+            this.Name = "MessageForm";
+            this.Text = "A Message For You";
+            this.Load += new System.EventHandler(this.MessageForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
